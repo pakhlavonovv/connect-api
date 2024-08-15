6214e790be65bf00ae40b8ab98424b4a57355ffd
@webdeveloper130390 <abdullokhpakhlavonov@gmail.com>
2024-08-15 12:08:55 +0500

    some-changes

--- pages/photos/js/photos.js
+++ pages/photos/js/photos.js
@@ -1,32 +1,31 @@
 const result = document.getElementById("result")
+let users = []
 document.addEventListener("DOMContentLoaded", function(){
-    getUsers();
+    getPhotos();
 })
-async function getUsers(){
-    const res = await fetch("https://jsonplaceholder.typicode.com/photos")
-    const users = await res.json();
-    displayUsers(users);
+async function getPhotos(){
+    try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/photos?_start=10&_limit=10")
+        users = await res.json();
+        displayPhotos();
+    } catch (error) {
+        console.log(error)
+    }
 }
-function displayUsers(users){
+function displayPhotos(){
     result.innerHTML = ""
     users.forEach((el) => {
         let div = document.createElement("div")
+        div.className = 'div col-md-3 d-flex gap-8'
         div.innerHTML = `
-        <div class="wrapper">
-        <div class="card mt-2">
-        <div class="card-header">
-            <button><i class="fa-solid fa-user fa-lg"></i></button>
-        </div>
-        <div class="card-body">
-            <div id="result">
-                <li>id: ${el.id}</li>
-                <li>title: ${el.title}</li>
-            <li>url: ${el.url}</li>
-            <li>thumbnailUrl: ${el.thumbnailUrl}</li>
+            <div class="card">
+            <div class="card-body ">
+            <img src='${el.url}' alt='${el.title}' class="w-100 h-100"/>
             </div>
-        </div>
-        </div>
-       
+            <div class="card-footer">
+            <p class="text-center">${el.title}</p>
+            </div>
+            </div>
         `
         result.appendChild(div)
     })
